e231e38f650be5692429ca74054d8bde081fcc8d
KeshavMowar711 <113666495+KeshavMowar711@users.noreply.github.com>
2026-06-15 14:32:06 +0530

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.cpp
@@ -0,0 +1,24 @@
+
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) 
+    {
+         if (!root) return true;
+
+        int left = height(root->left);
+        int right = height(root->right);
+        if (abs(left - right) > 1) return false;
+        return isBalanced(root->left) && isBalanced(root->right);
+    }
+
+    int height(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+
+        return 1 + max(height(root->left), height(root->right));
+
+        
+    }
+};
